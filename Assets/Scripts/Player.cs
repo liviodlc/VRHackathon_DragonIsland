cf5b967734905acb1688831a47e849725231bc94
Kelley Chen <kelley97@uw.edu>
2016-04-23 14:56:07 -0700

    added fields, added IsAlive method

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player {
+	private Room currentRoom;
+	private int health;
+	private Item items;
 
-	// Use this for initialization
-	void Start () {
-	
+	public Player() {
+		health = 10;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public bool IsAlive() {
+		return health == 0;
 	}
+
+
 }
