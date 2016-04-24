0d7f0a9c0418e4f1eeba8db420aeee02fd59cf7a
Livio De La Cruz <livio@delacruz.tech>
2016-04-23 17:03:20 -0700

    latest commit

--- Assets/Scripts/Content.cs
+++ Assets/Scripts/Content.cs
@@ -8,7 +8,12 @@ public class Content
 
 	public static void initContent(Player player, Room startingRoom)
 	{
-		Room r1 = new Room();
-		//startingRoom.setExit(r1);
+		Room r1 = startingRoom;
+
+		Room r2 = new Room();
+		r1.setExit(Direction.East, r1);
+
+		Room r3 = new Room();
+		r2.setExit(Direction.South, r3);
 	}
 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,15 +5,17 @@ public class Main : MonoBehaviour {
 
 	Player player;
 	Room currentRoom;
+	ArrayList roomStuff;
 
 	public GameObject ExitPrefab;
 	public float ExitDistance = 6f;
 
 	// Use this for initialization
 	void Start () {
+		roomStuff = new ArrayList();
 		player = new Player();
 		Room firstRoom = new Room();
-		Content.initContent(player, currentRoom);
+		Content.initContent(player, firstRoom);
 		setRoom(firstRoom);
 	}
 	
@@ -27,6 +29,10 @@ public class Main : MonoBehaviour {
 		if(currentRoom != null)
 		{
 			//clear current room
+			foreach(GameObject obj in roomStuff)
+			{
+				Destroy(obj);
+			}
 		}
 		//init current room
 		currentRoom = newRoom;
@@ -38,7 +44,26 @@ public class Main : MonoBehaviour {
 		foreach(Room.Exit exit in currentRoom.exits)
 		{
 			GameObject door = Instantiate(ExitPrefab);
-			
+			//door.transform.position = new Vector3(0, 0, 0);
+			switch(exit.d)
+			{
+				case Direction.North:
+					door.transform.position += new Vector3(0, 0, ExitDistance);
+					break;
+				case Direction.South:
+					door.transform.position += new Vector3(0, 0, -ExitDistance);
+					break;
+				case Direction.East:
+					door.transform.position += new Vector3(ExitDistance, 0, 0);
+					door.transform.Rotate(new Vector3(0, 90f, 0));
+					break;
+				case Direction.West:
+					door.transform.position += new Vector3(-ExitDistance, 0, 0);
+					door.transform.Rotate(new Vector3(0, -90f, 0));
+					break;
+			}
+			door.GetComponent<ExitToRoom>().myRoom = exit.r;
+			roomStuff.Add(door);
 		}
 
 	}
