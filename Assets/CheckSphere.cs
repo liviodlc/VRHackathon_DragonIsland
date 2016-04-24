690fd35184202b331effc5bc2e0b3ce282e9f229
Kelley Chen <kelley97@uw.edu>
2016-04-24 08:56:44 -0700

    ball throw

--- Assets/CheckSphere.cs
+++ Assets/CheckSphere.cs
@@ -6,6 +6,13 @@ public class CheckSphere : MonoBehaviour {
 	public Main main;
 	public Item myItem;
 
+
+	public Rigidbody rb;
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 	public void OnHighlight(bool state)
 	{
 		GetComponent<Renderer>().material.color = state ? Color.green : Color.black;
@@ -16,5 +23,12 @@ public class CheckSphere : MonoBehaviour {
 	public void onCommand() {
 		transform.parent = main.head.transform;
 		transform.localPosition = new Vector3(0.23f, -0.37f, 0.41f);
+//		rb.AddForce (0, 200, 200, ForceMode.Impulse);
+		rb.velocity = new Vector3(0, 50, 50);
+
+
+
 	}
+
+
 }
