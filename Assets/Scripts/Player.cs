d2bcaf077ba71d693424089ff31e26d5f7b18846
Kelley Chen <kelley97@uw.edu>
2016-04-23 17:03:56 -0700

    add current selection, add damage

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player {
+public class Player //Kyseen  {
 	private Room currentRoom;
 	private int health;
 	private Item items;
-
+	public Item currentSelection;
 	public Player() {
 		health = 10;
 	}
 
+	// True if alive, false if otherwise
 	public bool IsAlive() {
 		return health == 0;
+	}
+
+	// Picks up the item currently looking at
+	public void PickUp(Item currentSelection) {
+
+	}
+
+	// Takes health damage 
+	public void Damage() {
+		health--;
 	}
 
 
