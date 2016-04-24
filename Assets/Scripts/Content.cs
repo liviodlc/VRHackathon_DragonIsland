dda3a8c3322c8564df9e3da0fdf88e2730461c98
pinktreeleaf <pinktreeleaf@gmail.com>
2016-04-24 11:05:44 -0700

    "room 3-5"

--- Assets/Scripts/Content.cs
+++ Assets/Scripts/Content.cs
@@ -6,18 +6,33 @@ using System.Text;
 public class Content
 {
 
-	public static void initContent(Player player, Room startingRoom)
+	public static void initContent (Player player, Room startingRoom)
 	{
 		Room r1 = startingRoom;
+		Ball ball = new Ball ();
 
-		Room r2 = new Room();
-		Ball ball = new Ball();
-		r1.setExit(Direction.East, r2);
-		r1.addItem(ball);
+		Room r2 = new Room ();
+		Ball b2 = new Ball ();
 
-		Room r3 = new Room();
-		Ball b2 = new Ball();
-		r2.setExit(Direction.South, r3);
-		r2.addItem(b2);
+		Room r3 = new Room ();
+		Room r4 = new Room ();
+		Room r5 = new Room ();
+
+
+
+	
+
+		//Room 1
+		r1.setExit (Direction.East, r2);
+		r1.addItem (ball);
+
+		//#room2
+		r2.setExit (Direction.East, r3);
+		r2.addItem (b2);
+
+		//Room3
+		r3.setExit (Direction.North, r4);
+		r3.setExit (Direction.East, r5);
+
 	}
 }
