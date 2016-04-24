80eb4db81c33a1df507175712913d7f81c7c2aa1
Livio De La Cruz <livio@delacruz.tech>
2016-04-24 09:28:17 -0700

    throw ball on space

--- Assets/CheckSphere.cs
+++ Assets/CheckSphere.cs
@@ -6,11 +6,20 @@ public class CheckSphere : MonoBehaviour {
 	public Main main;
 	public Item myItem;
 	private Rigidbody rb;
+	private bool pickedUp = false;
 
 	void Start() 
 	{
 		rb = GetComponent<Rigidbody>();
+		rb.isKinematic = true;
 	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			onThrow();
+	}
+
 	public void OnHighlight(bool state)
 	{
 		GetComponent<Renderer>().material.color = state ? Color.green : Color.black;
@@ -21,13 +30,18 @@ public class CheckSphere : MonoBehaviour {
 	public void onCommand() {
 		transform.parent = main.head.transform;
 		transform.localPosition = new Vector3(0.23f, -0.37f, 0.41f);
-		//		rb.AddForce (0, 200, 200, ForceMode.Impulse);
+//		rb.AddForce (0, 200, 200, ForceMode.Impulse);
+		pickedUp = true;
 	}
 
 	public void onThrow()
 	{
-		transform.parent = null;
-		rb.velocity = (transform.position - main.head.transform.position) * 5;
+		if (pickedUp)
+		{
+			rb.isKinematic = false;
+			transform.parent = null;
+			rb.velocity = (transform.position - main.head.transform.position) * 10;
+		}
 	}
 
 
